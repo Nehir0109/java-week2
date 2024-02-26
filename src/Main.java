686f6e6254c7f5d466600463bbc689d56a91625c
Nehir0109 <nehir.cakilci@gmail.com>
2024-02-26 11:17:47 +0300

    Soru 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 //Soru 1: Verilen bir cümlede kaç adet boşluk karakteri bulunduğunu bulan bir Java programı yazın.
-public class Main {
+/*public class Main {
     public static void main(String[] args) {
         Scanner get = new Scanner(System.in);
 
@@ -18,5 +18,32 @@ public class Main {
         System.out.println(bosluk);
 
     }
-}
+}*/
 
+//Soru 2:Bir kullanıcıdan alınan bir kelimenin palindrom olup olmadığını kontrol eden bir Java programı yazın.
+// (Örneğin, "kayak" kelimesi bir palindromdur.)
+
+public class Main {
+    public static void main(String[] args){
+        Scanner get = new Scanner(System.in);
+
+        System.out.print("Bir kelime girin: ");
+        String kelime = get.nextLine();
+
+        boolean palindrom = true;
+        int uzunluk = kelime.length();
+
+        for(int i=0; i<uzunluk/2; i++){
+            if(kelime.charAt(i) != kelime.charAt(uzunluk - i-1)){
+                palindrom = false;
+                break;
+            }
+        }
+
+        if(palindrom){
+            System.out.println(kelime + " bir palindromdur.");
+        }else {
+            System.out.println(kelime + " bir palindrom değildir.");
+        }
+    }
+}
